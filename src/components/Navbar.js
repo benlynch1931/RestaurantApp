1d038257fa5e3a39a19a21123244527a98863819
Ben <ben.lynch1931@gmail.com>
2021-05-13 01:04:27 +0100

    add touchableopacity for `home` to top navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { AppContext } from '../contexts/AppContext.js';
 
 const Navbar = (props) => {
 
-  const { title, total, basket, removeFromBasket, removeFromTotal } = useContext(AppContext);
+  const { title, total, basket, removeFromBasket, removeFromTotal, setSection, setTitle } = useContext(AppContext);
   const [displayBasket, setDisplayBasket] = useState('none')
   
   const removeItem = (itemName, itemPrice) => {
@@ -43,16 +43,15 @@ const Navbar = (props) => {
         </View>
       )
     });
-    return render
-    
+    return render  
   }
   
   const renderBasket = () => {
     return (
       <View style={[{ position: 'absolute', width: wp('90%'), height: hp('77.5%'), display: displayBasket, zIndex: 10, backgroundColor: '#FFFFFF', marginTop: -hp('80%'), marginLeft: wp('5%') }]}>
         <View style={{ width: wp('90%'), height: hp('7.5%'), backgroundColor: '#919191' }}>
-          <Text style={{ fontSize: hp('5%'), textAlign: 'center', color: '#DBDBDB' }}>Tab</Text>
-          <TouchableOpacity style={{ position: 'absolute', right: wp('2%') }} onPress={() => { setDisplayBasket('none') }}><Text style={{ fontSize: hp('4%'), color: '#DBDBDB' }}>X</Text></TouchableOpacity>
+          <Text style={{ fontSize: hp('5%'), textAlign: 'center', color: '#DBDBDB', marginTop: 0, lineHeight: hp('5%'), marginTop: hp('1.25%') }}>Tab</Text>
+          <TouchableOpacity style={{ position: 'absolute', right: wp('2%') }} onPress={() => { setDisplayBasket('none') }}><Text style={{ fontSize: hp('4%'), color: '#DBDBDB', lineHeight: hp('4%'), marginTop: hp('1.75%') }}>X</Text></TouchableOpacity>
         </View>
         
         <ScrollView style={{ width: wp('90%'), height: hp('60%'), paddingTop: hp('1%') }}>{ iterateBasketItems() }</ScrollView>
@@ -65,8 +64,11 @@ const Navbar = (props) => {
 
     return (
       <View style={{ position: 'fixed', top: 0, width: wp('100%'), height: hp('10%'), backgroundColor: '#FFFFFF', shadowColor: '#919191', shadowOffset: { height: 10, width: 0 }, shadowRadius: 4, shadowOpacity: 1}}>
-        <Image source={require('../../assets/menu.png')} style={{ width: hp('9%'), height: hp('9%'), marginTop: hp('1.25%'), position: 'absolute' }} />
+        <Image source={require('../../assets/menu.png')} style={{ width: hp('9%'), height: hp('9%'), marginTop: hp('1.25%'), position: 'absolute', left: wp('0%') }} />
         <Text style={{ textAlign: 'center', marginTop: hp('3.25%'), fontSize: hp('3.5%') }}>{ title }</Text>
+        <TouchableOpacity onPress={() => { setSection('home'); setTitle('Home') }} style={{ width: hp('4.5%'), height: hp('4.5%'), marginTop: hp('3.5%'), position: 'absolute', right: wp('4.5%') }}>
+          <Image source={require('../../assets/home.png')} style={{ width: hp('4.5%'), height: hp('4.5%') }} />
+        </TouchableOpacity>
       </View>
     )
   } else if (props.location == 'bottom') {
