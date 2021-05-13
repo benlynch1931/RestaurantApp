b0f3ce571ddfefec7abfc588628a98ea55dd0b16
Ben <ben.lynch1931@gmail.com>
2021-05-13 18:49:07 +0100

    fix `splice()` issue with exporting tabs

--- src/screens/tabs/MainTabs.js
+++ src/screens/tabs/MainTabs.js
@@ -10,31 +10,35 @@ import { TabContext } from '../../contexts/TabContext.js';
 const MainTabs = (props) => {
 
   const { setSection, setTitle, basket, total, removeFromTotal, removeFromBasket, addToTotal, addToBasket } = useContext(AppContext);
-  const { tabs, addTab, isCurrentTab, currentTabNumber, currentTabName, setIsCurrentTab, setCurrentTabNumber, setCurrentTabName } = useContext(TabContext);
+  const { tabs, addTab, isCurrentTab, currentTabNumber, currentTabName, setIsCurrentTab, setCurrentTabNumber, setCurrentTabName, viewTabInfo, setViewTabInfo } = useContext(TabContext);
   const [ createTabRendering, setCreateTabRendering ] = useState('none')
+  const [ viewTabRendering, setViewTabRendering ] = useState('none')
   const [ newTabNumber, setNewTabNumber ] = useState('1')
   const [ newTabName, setNewTabName ] = useState(`Table ${newTabNumber}`)
+  const [ test, setTest ] = useState(null)
   
-  const [test, setTest] = useState(null)
   
   
   const exportTab = (selectedTab) => {
-    let tabIndexToExport = 0
-    tabs.forEach((tab, idx) => {
-      if (tab == selectedTab) {
-        tabIndexToExport = idx
-      }
-    });
+    let tabIndexToExport = tabs.indexOf(selectedTab)
+    // setTest(tabIndexToExport)
+    // setTest(JSON.stringify(tabs))
     addToTotal(tabs[tabIndexToExport].total)
     addToBasket(tabs[tabIndexToExport].basket)
     setIsCurrentTab(true)
     setCurrentTabNumber(tabs[tabIndexToExport].number)
     setCurrentTabName(tabs[tabIndexToExport].name)
-    let removedTab = []
+    // let removedTab = []
+    
     if (tabs.length > 1) {
-      removedTab = tabs.splice(tabIndexToExport, 1)
+      let tabToAlter = tabs
+      tabToAlter.splice(tabIndexToExport, 1)
+      addTab(tabToAlter)
+    } else {
+      addTab([])
     }
-    addTab(removedTab)
+    // setTest(JSON.stringify(removedTab))
+    
   }
   
   const importToTab = (selectedTab) => {
@@ -60,8 +64,8 @@ const MainTabs = (props) => {
     addTab([
       ...tabs,
       {
-        name: newTabName,
-        number: newTabNumber,
+        name: newTabName || currentTabName,
+        number: newTabNumber || currentTabNumber,
         basket: basket,
         total: total
       }
@@ -76,18 +80,17 @@ const MainTabs = (props) => {
     setCreateTabRendering('none')
   }
   
-  const closeCreatTabRender = () => {
+  const closeCreateTabRender = () => {
     setNewTabName('')
     setNewTabNumber('')
     setCreateTabRendering('none')
-    
   }
   
   const renderCreateTab = () => {
     return (
       <View style={{ display: createTabRendering, width: wp('100%'), height: hp('82.5%'), backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'absolute' }}>
         <View style={{ position: 'absolute', right: wp('5%'), top: wp('4%') }}>
-          <TouchableOpacity onPress={() => { closeCreatTabRender() }}>
+          <TouchableOpacity onPress={() => { closeCreateTabRender() }}>
             <Text style={{ fontSize: hp('5%'), color: '#FFFFFF' }}>X</Text>
           </TouchableOpacity>
         </View>
@@ -118,6 +121,69 @@ const MainTabs = (props) => {
         
       </View>
     )
+  }
+  
+  const viewTab = (selectedTab) => {
+    setViewTabInfo(selectedTab)
+    setViewTabRendering('block')
+    setNewTabName(currentTabName)
+    setNewTabNumber(currentTabNumber)
+  }
+  
+  const closeViewTabInfoRender = () => {
+    setViewTabInfo({
+      name: '',
+      number: '',
+      basket: [],
+      total: 0
+    })
+    setViewTabRendering('none')
+  }
+  
+  const renderViewTabInfo = () => {
+    return (
+      <View style={{ display: viewTabRendering, width: wp('100%'), height: hp('82.5%'), backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'absolute', zIndex: 9 }}>
+        <View style={{ position: 'absolute', right: wp('5%'), top: wp('4%') }}>
+          <TouchableOpacity onPress={() => { closeViewTabInfoRender() }}>
+            <Text style={{ fontSize: hp('5%'), color: '#FFFFFF' }}>X</Text>
+          </TouchableOpacity>
+        </View>
+      
+        <View style={styles.viewTabWindowStyle}>
+          <Text style={{ position: 'absolute', fontSize: hp('3%'), left: wp('2.5%'), top: wp('2.5%'), lineHeight: hp('3%') }}>{ viewTabInfo.number }</Text>
+          <Text style={{ fontSize: hp('6%'), textAlign: 'center', lineHeight: hp('6%'), marginTop: hp('2%') }}>{ viewTabInfo.name }</Text>
+          
+          <View style={[{ width: wp('80%'), height: hp('58%'), zIndex: 10, backgroundColor: '#FFFFFF' }]}>
+            
+            <ScrollView style={{ width: wp('80%'), height: hp('62%'), paddingTop: hp('1%') }}>{ iterateTabItems() }</ScrollView>
+            <View style={{ position: 'absolute', width: wp('90%'), height: hp('5%'), bottom: hp('5%'), paddingRight: wp('5%') }}><Text style={{ textAlign: 'right', fontSize: hp('3%'), right: wp('10%') }}>£ { viewTabInfo.total.toFixed(2) }</Text></View>
+          </View>
+          
+          
+        </View>
+        
+      </View>
+    )
+  }
+  
+  const iterateTabItems = () => {
+    let render = []
+    viewTabInfo.basket.forEach((item, idx) => {
+      render.push(
+        <View style={{ position: 'relative', height: hp('5%') }}>
+
+          
+          <View style={{ position: 'absolute', left: wp('5%') }}>
+            <Text style={{ fontSize: hp('3%') }}>1 x {item[0]}</Text>
+          </View>
+          
+          <View style={{ position: 'absolute', right: wp('5%') }}>
+            <Text style={{ fontSize: hp('3%') }}>£ {item[1].toFixed(2)}</Text>
+          </View>
+        </View>
+      )
+    });
+    return render  
   }
   
   const renderCreateTabButton = () => {
@@ -153,11 +219,11 @@ const MainTabs = (props) => {
         rendering.push(
           <View style={{ width: wp('90%'), height: hp('7.5%'), backgroundColor: '#919191', marginTop: hp('2.5%'), marginLeft: wp('5%') }}>
           
-            <TouchableOpacity style={{ position: 'absolute', marginLeft: wp('2%'), maxWidth: wp('30%') }}>
+            <TouchableOpacity style={{ position: 'absolute', marginLeft: wp('2%'), maxWidth: wp('30%') }} onPress={() => { viewTab(tab) }}>
               <Text style={[styles.textStyle]}>{tab.name}</Text>
             </TouchableOpacity>
             
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => { viewTab(tab) }}>
               <Text style={[styles.textStyle, { textAlign: 'center' }]}>£ {tab.total.toFixed(2)}</Text>
             </TouchableOpacity>
             
@@ -193,8 +259,6 @@ const MainTabs = (props) => {
   }
   
   const updateTab = () => {
-    setNewTabName(currentTabName)
-    setNewTabNumber(currentTabNumber)
     createTab()
   }
 
@@ -217,7 +281,11 @@ const MainTabs = (props) => {
       </View>
       
       { renderCreateTab() }
+      { renderViewTabInfo() }
       { renderTabs() }
+      
+      <Text>{ test }</Text>
+      
       
     </View>
   )
@@ -282,6 +350,20 @@ const styles = StyleSheet.create({
     position: 'absolute',
     textAlign: 'right', 
     width: wp('30%') 
+  },
+  viewTabWindowStyle: { 
+    width: wp('90%'),
+    height: hp('70%'),
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    top: hp('10%'),
+    left: wp('5%'),
+    padding: wp('5%') ,
+    shadowColor: '#919191',
+    shadowRadius: 5,
+    shadowOpacity: 1,
+    shadowOffset: { height: 0 } ,
+    zIndex: 9
   }
 });
 
