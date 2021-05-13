996d9dbedecf8eaa9ace3bc53ee36477cb0b343b
Ben <ben.lynch1931@gmail.com>
2021-05-13 01:02:54 +0100

    add Tab context

--- App.js
+++ App.js
@@ -6,14 +6,17 @@ import Navbar from './src/components/Navbar';
 import Main from './src/controllers/Main.js';
 
 import AppContextProvider from './src/contexts/AppContext.js';
+import TabContextProvider from './src/contexts/TabContext.js';
 
 export default function App() {
   return (
     <View style={{ width: '100%', backgroundColor: '#E9E9E9', overflow: 'scroll' }}>
       <AppContextProvider>
+      <TabContextProvider>
         <Navbar location={'top'} />
         <Main />
         <Navbar location={'bottom'} />
+      </TabContextProvider>
       </AppContextProvider>
     </View>
   );
