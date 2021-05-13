75d4760fda1aafe678f906a64d2803b179a7bdd1
Ben <ben.lynch1931@gmail.com>
2021-05-13 01:04:50 +0100

    add to tabcontroller

--- src/controllers/TabController.js
+++ src/controllers/TabController.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+import MainTabs from '../screens/tabs/MainTabs.js';
+
+
 
 
 const TabController = () => {
   
   return (
-    null
+    <View>
+      <MainTabs />
+    </View>
   )
 }
 
