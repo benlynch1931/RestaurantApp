6e6856486c688a5b226d45f61501dab680245619
Ben <ben.lynch1931@gmail.com>
2021-05-23 13:25:01 +0100

    home and department screen rendering from database iteration

---
+++ src/screens/DepartmentScreen.js
@@ -0,0 +1,85 @@
+import React, { useContext, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+import styles from '../styles/drinks.js'
+
+import { AppContext } from '../contexts/AppContext.js'
+
+const DepartmentScreen = (props) => {
+  
+  const { departments, setDepartments } = useContext(AppContext);
+  
+  const fetchDepartments = () => {
+    fetch('http://192.168.1.213:6030/api/departments', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setDepartments(data.departments)
+    })
+  }
+  
+  const getSpecificDepartments = (groupID) => {
+    let specificDepartments = [];
+    departments.forEach((item, idx) => {
+      if (item.group.index === groupID) {
+        specificDepartments.push(item)
+      }
+    });
+    return specificDepartments;
+  }
+  
+  const renderDepartments = (groupID) => {
+    const specificDepartments = getSpecificDepartments(groupID)
+    let rendering = [];
+    for (let idx=0; idx<specificDepartments.length; idx+=2) {
+      // if can render two departments in a row
+      if (idx <= specificDepartments.length - 2) {
+        rendering.push(
+          <View style={styles.rowStyle}>
+            <View style={{ ...styles.optionStyle, marginRight: wp('10%') }}>
+              <TouchableOpacity style={styles.touchableStyle} onPress={() => {  }}>
+                <Text style={styles.textStyle}>{ specificDepartments[idx].name }</Text>
+              </TouchableOpacity>
+            </View>
+            
+            <View style={styles.optionStyle}>
+              <TouchableOpacity style={styles.touchableStyle} onPress={() => {  }}>
+                <Text style={{ ...styles.textStyle }}>{ specificDepartments[idx+1].name }</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )
+      } else {
+        rendering.push(
+          <View style={styles.rowStyle}>
+            <TouchableOpacity style={styles.touchableStyle} onPress={() => {  }}>
+              <Text style={{ ...styles.textStyle }}>{ specificDepartments[idx].name }</Text>
+            </TouchableOpacity>
+          </View>
+        )
+      }
+    }
+    return rendering;
+    
+  }
+  
+  useEffect(() => {
+    fetchDepartments()
+  }, [])
+  
+  return (
+    <ScrollView style={{ width: wp('100%'), height: hp('82.5%'), backgroundColor: '#FFFFFF' }}>
+    
+      { renderDepartments(2) }
+    
+    </ScrollView>
+  )
+}
+
+export default DepartmentScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -7,7 +7,7 @@ import { AppContext } from '../contexts/AppContext.js';
 
 const HomeScreen = (props) => {
 
-  const { setSection, setTitle } = useContext(AppContext);
+  const { setSection, setTitle, setGroups, groups } = useContext(AppContext);
   
   const optionStyle = {
     width: wp('80%'),
@@ -18,35 +18,81 @@ const HomeScreen = (props) => {
     shadowOpacity: 1,
     shadowOffset: { height: 0 } 
   }
+  
+  const fetchGroups = () => {
+    fetch('http://192.168.1.213:6030/api/groups', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setGroups(data.groups)
+    })
+  }
+  
+  const renderGroups = () => {
+    let rendering = [];
+    groups.forEach((group, idx) => {
+      rendering.push(
+        <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
+          <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }} onPress={() => {  }}>
+            <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>{ group.group }</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    });
+    rendering.push(
+      <View style={{ ...optionStyle, marginLeft: wp('10%'), marginBottom: hp('5%') }}>
+        <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }} onPress={() => { setSection('tab'); setTitle('Tabs') }}>
+          <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>TABS</Text>
+        </TouchableOpacity>
+      </View>
+    )
+    return rendering;
+  }
+  
+  useEffect(() => {
+    fetchGroups()
+  }, [])
 
 
   return (
     <ScrollView style={{ width: wp('100%'), backgroundColor: '#E9E9E9', height: hp('82.5%') }}>
-      
-      <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
-        <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }} onPress={() => { setSection('drink'); setTitle('Drinks') }}>
-          <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>Drinks</Text>
-        </TouchableOpacity>
-      </View>
-      
-      <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
-        <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }}>
-          <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>Food</Text>
-        </TouchableOpacity>
-      </View>
-      
-      <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
-        <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }} onPress={() => { setSection('tab'); setTitle('Tabs') }}>
-          <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>Tabs</Text>
-        </TouchableOpacity>
-      </View>
-      
-      {/*<View style={{ width: wp('100%'), height: hp('10%'), marginTop: hp('5%'), backgroundColor: '#FFFFFF'}}>
-        <Text style={{ textAlign: 'center' }}>User</Text>
-      </View>*/}
-      
+    
+      { renderGroups() }
+    
     </ScrollView>
   )
+
+  // return (
+  //   <ScrollView style={{ width: wp('100%'), backgroundColor: '#E9E9E9', height: hp('82.5%') }}>
+  // 
+  //     <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
+  //       <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }} onPress={() => { setSection('drink'); setTitle('Drinks') }}>
+  //         <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>Drinks</Text>
+  //       </TouchableOpacity>
+  //     </View>
+  // 
+  //     <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
+  //       <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }}>
+  //         <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>Food</Text>
+  //       </TouchableOpacity>
+  //     </View>
+  // 
+  //     <View style={{ ...optionStyle, marginLeft: wp('10%') }}>
+  //       <TouchableOpacity style={{ width: wp("80%"), paddingTop: hp('7.5%'), paddingBottom: hp('7.5%') }} onPress={() => { setSection('tab'); setTitle('Tabs') }}>
+  //         <Text style={{ alignSelf: 'center', fontSize: hp('5%') }}>Tabs</Text>
+  //       </TouchableOpacity>
+  //     </View>
+  // 
+  //     {/*<View style={{ width: wp('100%'), height: hp('10%'), marginTop: hp('5%'), backgroundColor: '#FFFFFF'}}>
+  //       <Text style={{ textAlign: 'center' }}>User</Text>
+  //     </View>*/}
+  // 
+  //   </ScrollView>
+  // )
 }
 
 const styles = StyleSheet.create({
